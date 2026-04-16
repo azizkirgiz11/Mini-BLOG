b279afeaa2eeacf9502a81fba6fbde9b1dc8bbee
Aruukee <nurbekvvnass@example.com>
2026-04-16 11:40:56 +0600

    add firebase

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { db } from '../firebase'
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
-const Comment = () => {
+const Comment = ({postId}) => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+
+    try {
+  await addDoc(collection(db,
+    "comments"), {
+      postId: postId, 
+      text: text,
+      createAt: serverTimestamp(),
+      userName: "welcome"
+    });
+    setText('');
+  } catch (error) {
+    console.error("Error addingdocument:", error);
+  }
+  };
+
   return (
-    <div>Comment</div>
+    <form onSubmit={handleSubmit} className="mt-4">
+      <textarea className="w-full p-2 border rounded-lg focus:ring-2 focus:ring-purple-500 outline-none"
+      placeholder="Express your opinion"
+      value={text}
+      onChange={(e) => 
+        setText(e,EventTarget.value)}
+        />
+      <button className="bg-purple-600
+      text-white px-4 py-2 mt-2 rounded-md hover:bg-purple-700">
+        Send
+      </button>
+    </form>
   )
-}
+};
 
 export default Comment
